4efeab6163a0eabfa605dc450bd6281ff2d39b91
BeBoXoS <33153857+beboxos@users.noreply.github.com>
2018-10-03 09:28:11 +0200

    Petite app en VB pour créer des constantes
    prends un texte et crée le code pour le mettre en constante dans un code

---
+++ ConstTextImporter/ConstTextImporter/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBoxVarName = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBoxSource = New System.Windows.Forms.TextBox()
+        Me.TextBoxDestination = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Nom de la variable"
+        '
+        'TextBoxVarName
+        '
+        Me.TextBoxVarName.Location = New System.Drawing.Point(113, 6)
+        Me.TextBoxVarName.Name = "TextBoxVarName"
+        Me.TextBoxVarName.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxVarName.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(41, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Source"
+        '
+        'TextBoxSource
+        '
+        Me.TextBoxSource.Location = New System.Drawing.Point(15, 53)
+        Me.TextBoxSource.Multiline = True
+        Me.TextBoxSource.Name = "TextBoxSource"
+        Me.TextBoxSource.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxSource.Size = New System.Drawing.Size(773, 119)
+        Me.TextBoxSource.TabIndex = 3
+        '
+        'TextBoxDestination
+        '
+        Me.TextBoxDestination.Location = New System.Drawing.Point(12, 191)
+        Me.TextBoxDestination.Multiline = True
+        Me.TextBoxDestination.Name = "TextBoxDestination"
+        Me.TextBoxDestination.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBoxDestination.Size = New System.Drawing.Size(776, 247)
+        Me.TextBoxDestination.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 175)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(60, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Destination"
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(713, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Créer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(219, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(131, 23)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Importer un fichier texte"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Location = New System.Drawing.Point(632, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Reset"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBoxDestination)
+        Me.Controls.Add(Me.TextBoxSource)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBoxVarName)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Constant Text Creator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBoxVarName As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBoxSource As TextBox
+    Friend WithEvents TextBoxDestination As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents Button3 As Button
+End Class
---
+++ ConstTextImporter/ConstTextImporter/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim fichier As String = ""
+        OpenFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"
+        OpenFileDialog1.ShowDialog()
+        If OpenFileDialog1.FileName <> "" Then
+            TextBoxSource.Text = IO.File.ReadAllText(OpenFileDialog1.FileName)
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TextBoxDestination.Text = ""
+        TextBoxSource.Text = ""
+        TextBoxVarName.Text = ""
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBoxVarName.Text = "" Then
+            MsgBox("Nom de la variable non renseigné", vbCritical)
+            Exit Sub
+        End If
+        If TextBoxSource.Text = "" Then
+            MsgBox("Aucun texte source", vbCritical)
+            Exit Sub
+        End If
+        Dim tempo As Array = Split(TextBoxSource.Text, vbCrLf)
+        TextBoxDestination.Text = "Dim " & TextBoxVarName.Text & " as String = vbEmpty" & vbCrLf
+        For n = LBound(tempo) To UBound(tempo) - 1
+            TextBoxDestination.Text = TextBoxDestination.Text & TextBoxVarName.Text & " = " & TextBoxVarName.Text & " & """ & tempo(n).ToString.Replace("""", """""") & """ & vbCrLf" & vbCrLf
+        Next n
+    End Sub
+
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        TextBoxSource.Width = Me.Width - 43
+        TextBoxDestination.Width = Me.Width - 43
+        TextBoxDestination.Height = Me.Height - 241
+    End Sub
+End Class
