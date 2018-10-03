29eb0e42a7dc386e58acb1793f0dc016d2c2dbca
BeBoXoS <33153857+beboxos@users.noreply.github.com>
2018-10-03 09:23:20 +0200

    test encodage 3Des
    avec routine pour récupérer numéro de série hdd et mac adress de la carte

---
+++ 3Des Encoder Tests/SAPconnectorTest/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TxPassword = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 34)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(776, 249)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 339)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(776, 196)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Texte Brut"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(8, 315)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Texte encodé"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(709, 310)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Encoder"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(628, 310)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "decoder"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TxPassword
+        '
+        Me.TxPassword.Location = New System.Drawing.Point(271, 312)
+        Me.TxPassword.Name = "TxPassword"
+        Me.TxPassword.Size = New System.Drawing.Size(128, 20)
+        Me.TxPassword.TabIndex = 6
+        Me.TxPassword.UseSystemPasswordChar = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(212, 315)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Password"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(114, 310)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 547)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxPassword)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Encodage 3DES"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TxPassword As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button3 As Button
+End Class
---
+++ 3Des Encoder Tests/SAPconnectorTest/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.Security.Cryptography
+Imports System.Net.NetworkInformation
+Public Class Form1
+    Public NotInheritable Class Simple3Des
+        Private TripleDes As New TripleDESCryptoServiceProvider
+        Private Function TruncateHash(
+            ByVal key As String,
+            ByVal length As Integer) As Byte()
+            Dim sha1 As New SHA1CryptoServiceProvider
+
+            ' Hash the key.
+            Dim keyBytes() As Byte =
+                System.Text.Encoding.Unicode.GetBytes(key)
+            Dim hash() As Byte = sha1.ComputeHash(keyBytes)
+
+            ' Truncate or pad the hash.
+            ReDim Preserve hash(length - 1)
+            Return hash
+        End Function
+        Sub New(ByVal key As String)
+            ' Initialize the crypto provider.
+            TripleDes.Key = TruncateHash(key, TripleDes.KeySize \ 8)
+            TripleDes.IV = TruncateHash("", TripleDes.BlockSize \ 8)
+        End Sub
+        Public Function EncryptData(
+    ByVal plaintext As String) As String
+
+            ' Convert the plaintext string to a byte array.
+            Dim plaintextBytes() As Byte =
+                System.Text.Encoding.Unicode.GetBytes(plaintext)
+
+            ' Create the stream.
+            Dim ms As New System.IO.MemoryStream
+            ' Create the encoder to write to the stream.
+            Dim encStream As New CryptoStream(ms,
+                TripleDes.CreateEncryptor(),
+                System.Security.Cryptography.CryptoStreamMode.Write)
+
+            ' Use the crypto stream to write the byte array to the stream.
+            encStream.Write(plaintextBytes, 0, plaintextBytes.Length)
+            encStream.FlushFinalBlock()
+
+            ' Convert the encrypted stream to a printable string.
+            Return Convert.ToBase64String(ms.ToArray)
+        End Function
+        Public Function DecryptData(
+            ByVal encryptedtext As String) As String
+
+            ' Convert the encrypted text string to a byte array.
+            Dim encryptedBytes() As Byte = Convert.FromBase64String(encryptedtext)
+
+            ' Create the stream.
+            Dim ms As New System.IO.MemoryStream
+            ' Create the decoder to write to the stream.
+            Dim decStream As New CryptoStream(ms,
+                TripleDes.CreateDecryptor(),
+                System.Security.Cryptography.CryptoStreamMode.Write)
+
+            ' Use the crypto stream to write the byte array to the stream.
+            decStream.Write(encryptedBytes, 0, encryptedBytes.Length)
+            decStream.FlushFinalBlock()
+
+            ' Convert the plaintext stream to a string.
+            Return System.Text.Encoding.Unicode.GetString(ms.ToArray)
+        End Function
+    End Class
+
+
+    Sub TestEncoding()
+        Dim plainText As String = TextBox1.Text
+        Dim password As String = TxPassword.Text
+        TextBox2.Text = ""
+        Dim wrapper As New Simple3Des(password)
+        Dim cipherText As String = wrapper.EncryptData(plainText)
+        TextBox2.Text = cipherText
+        'MsgBox("The cipher text is: " & cipherText)
+        'My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\cipherText.txt", cipherText, False)
+        My.Computer.Clipboard.SetText(cipherText)
+    End Sub
+
+    Sub TestDecoding()
+        'Dim cipherText As String = My.Computer.FileSystem.ReadAllText(My.Computer.FileSystem.SpecialDirectories.MyDocuments & "\cipherText.txt")
+        Dim cipherText As String = TextBox2.Text
+        Dim password As String = TxPassword.Text
+        Dim wrapper As New Simple3Des(password)
+        TextBox1.Text = ""
+        ' DecryptData throws if the wrong password is used.
+        Try
+            Dim plainText As String = wrapper.DecryptData(cipherText)
+            TextBox1.Text = plainText
+            'MsgBox("The plain text is: " & plainText)
+        Catch ex As System.Security.Cryptography.CryptographicException
+            MsgBox("The data could not be decrypted with the password.")
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        TestEncoding()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        TestDecoding()
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        TxPassword.Text = ""
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        'Dim searcher As ManagementObjectSearcher = New ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMedia")
+        'MsgBox(test)
+        TxPassword.Text = GetDriveSerialNumber() & getMacAddress() ' met le numéro de serie du hdd comme pass par défaut concaténé avec la mac adress
+
+    End Sub
+
+    'recupere un process exemple netstat
+    'Dim truc As New System.Diagnostics.Process()
+    'truc.StartInfo.RedirectStandardOutput = True ' on redirige le flux de la console
+    'truc.StartInfo.UseShellExecute = False ' pour que le flux arrive vers notre textbox
+    'truc.StartInfo.CreateNoWindow = True ' true pour ne pas avoir la fenetre noire, false pour l'avoir
+    'truc.StartInfo.Arguments = ("-an")
+    'truc.StartInfo.FileName = ("C:\Windows\System32\NETSTAT.EXE")
+    'truc.Start()
+    'Me.TextBox1.Text = truc.StandardOutput.ReadToEnd ' on affiche le flux que nous avons préalablement redirigé
+    Public Function GetDriveSerialNumber() As String
+        Dim DriveSerial As Integer
+        'Create a FileSystemObject object
+        Dim fso As Object = CreateObject("Scripting.FileSystemObject")
+        Dim Drv As Object = fso.GetDrive(fso.GetDriveName(Application.StartupPath))
+        With Drv
+            If .IsReady Then
+                DriveSerial = .SerialNumber
+            Else    '"Drive Not Ready!"
+                DriveSerial = -1
+            End If
+        End With
+        Return DriveSerial.ToString("X2")
+    End Function
+    Function getMacAddress()
+        Dim nics() As NetworkInterface = NetworkInterface.GetAllNetworkInterfaces()
+        Return nics(1).GetPhysicalAddress.ToString
+    End Function
+
+End Class
